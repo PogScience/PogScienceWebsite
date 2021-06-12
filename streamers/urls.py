50ad168de39ccf25776a54abb15422cecd73f4fe
Amaury Carrade <amaury@carrade.eu>
2021-06-12 20:59:23 +0200

    Éléments préparatifs de test
    - Ajout de templates de base pour tester
    - Ajout d'une liste basique sur l'accueil

--- streamers/urls.py
+++ streamers/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
+from streamers.views import HomeView
+
+app_name = "streamers"
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("", HomeView.as_view(), name="home")
 ]
--- streamers/views.py
+++ streamers/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from streamers.models import Streamer
+
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+    extra_context = {
+        'streamers': Streamer.objects.all()
+    }
