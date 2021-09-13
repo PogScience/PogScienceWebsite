7c6be464a6f7ff60c79204c8657ee660ad7268cb
Amaury Carrade <amaury@carrade.eu>
2021-09-13 13:26:25 +0200

    Début de réflexion sur la présentation des lives sur l'accueil

--- streamers/views.py
+++ streamers/views.py
@@ -5,4 +5,7 @@ from streamers.models import Streamer
 
 class HomeView(TemplateView):
     template_name = "home.html"
-    extra_context = {"streamers": Streamer.objects.all()}
+    extra_context = {
+        "streamers": Streamer.objects.all(),
+        "live_streamers": list(Streamer.objects.filter(live=True))
+    }
