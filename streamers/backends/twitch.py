3e54606eb41853b91e0b98a4be78d11b55592d79
Amaury Carrade <amaury@carrade.eu>
2021-10-14 17:55:24 +0200

    Fix: update Twitch social-auth and fix unsaved refresh token
    - We use a fixed version of social-auth-core for now as that version
      contains a fix we need to work with Twitch (hence removing lots of
      our fixes for Twitch Helix API).
    - The RAID API now refreshes the token before use, if needed, else we
      can only raid channels four hours after login.

--- streamers/backends/twitch.py
+++ streamers/backends/twitch.py
@@ -1,5 +1,4 @@
 from social_core.backends.twitch import TwitchOAuth2 as PSATwitchOAuth2
-from django.conf import settings
 
 
 class TwitchOAuth2(PSATwitchOAuth2):
@@ -8,23 +7,10 @@ class TwitchOAuth2(PSATwitchOAuth2):
     supported), hence this updated version.
     """
 
-    ID_KEY = "id"
     DEFAULT_SCOPE = ["user:read:email", "chat:read", "chat:edit", "channel_editor"]
     STATE_PARAMETER = "state"
-
-    def get_user_details(self, response):
-        return {
-            "username": response.get("login"),
-            "email": response.get("email"),
-            "first_name": response.get("display_name"),
-            "last_name": "",
-        }
-
-    def user_data(self, access_token, *args, **kwargs):
-        return self.get_json(
-            "https://api.twitch.tv/helix/users",
-            headers={
-                "Client-Id": settings.SOCIAL_AUTH_TWITCH_KEY,
-                "Authorization": f"Bearer {access_token}",
-            },
-        )["data"][0]
+    EXTRA_DATA = [
+        ("access_token", "access_token"),
+        ("refresh_token", "refresh_token"),
+        ("expires_in", "expires"),
+    ]
--- streamers/views/raid.py
+++ streamers/views/raid.py
@@ -5,28 +5,48 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from rest_framework import views
 from rest_framework.request import Request
 from rest_framework.response import Response
+from social_django.models import UserSocialAuth
+from social_django.utils import load_strategy
+
+from streamers.backends.twitch import TwitchOAuth2
+
+
+def send_irc_message(sock: socket.socket, message: str):
+    """
+    Sends a IRC message over the given socket. Line breaks are appended and the
+    string is encoded to bytes.
+
+    :param sock: The socket.
+    :param message: The message.
+    """
+    sock.send(bytes(message + "\r\n", "UTF-8"))
 
 
 class RaidAPIView(LoginRequiredMixin, views.APIView):
-    def send(self, sock: socket.socket, message: str):
-        sock.send(bytes(message + "\r\n", "UTF-8"))
-
     def post(self, request: Request, **kwargs):
-        social = request.user.social_auth.get(provider="twitch")
+        social: UserSocialAuth = request.user.social_auth.get(provider="twitch")
         if not social:
             raise RuntimeError(f"OAuth social data missing for user {request.user.username}")
 
-        token = social.extra_data["access_token"]
+        token = social.get_access_token(load_strategy(request))
+
         context = ssl.create_default_context()
         twitch_irc_host = "irc.chat.twitch.tv", 6697
 
         with socket.create_connection(twitch_irc_host) as sock:
             with context.wrap_socket(sock, server_hostname=twitch_irc_host[0]) as ssock:
-                self.send(ssock, "CAP REQ :twitch.tv/commands")
-                self.send(ssock, f"PASS oauth:{token}")
-                self.send(ssock, f"NICK {request.user.username}")
-                self.send(ssock, f"USER {request.user.username} 8 * :{request.user.username}")
-                self.send(ssock, f"JOIN #{request.user.username}")
-                self.send(ssock, f"PRIVMSG #{request.user.username} :/raid {self.kwargs['twitch_login']}")
+                # Required to be able to send Twitch commands
+                send_irc_message(ssock, "CAP REQ :twitch.tv/commands")
+
+                # Authenticates to Twitch IRC
+                send_irc_message(ssock, f"PASS oauth:{token}")
+                send_irc_message(ssock, f"NICK {request.user.username}")
+                send_irc_message(ssock, f"USER {request.user.username} 8 * :{request.user.username}")
+
+                # Joins the stream chat's channel
+                send_irc_message(ssock, f"JOIN #{request.user.username}")
+
+                # Sends the /raid command
+                send_irc_message(ssock, f"PRIVMSG #{request.user.username} :/raid {self.kwargs['twitch_login']}")
 
         return Response(status=204)
